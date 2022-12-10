4ce092f9f2fb44277adf5c2bf9cb58226ae17132
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-12-10 22:51:20 +0600

    projects page edited

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -2,17 +2,36 @@ import React from 'react';
 
 const Projects = () => {
     return (
-		<div className='min-h-screen'>
-			<h1 className='text-4xl mb-20 text-pink-500 font-serif font-bold'>My All Projects</h1>
+			<div className='min-h-screen'>
+				<h1 className='text-4xl mb-20 text-pink-500 font-serif font-bold'>
+					My All Projects
+				</h1>
 				<div className='grid grid-cols-1 lg:grid-cols-3 gap-4 lg:ml-7'>
 					<div className='card w-96 bg-base-100 shadow-xl image-full'>
 						<figure>
 							<img src='photoShopCover.png' alt='Shoes' />
 						</figure>
 						<div className='card-body'>
-							<p>A website for hiring a photographer</p>
-							<div className='card-actions justify-end'>
-								<button className='btn btn-primary'>
+							<h1 className='text-lg text-yellow-400 text-start text-bold font-serif'>
+								A website for hiring a photographer
+							</h1>
+							<h2 className='text-lg text-pink-500 text-start text-bold font-serif'>
+								Technologies used in this project:
+							</h2>
+							<p className='text-start'>
+								React, React-Tailwindcss, React Router, TanStack Query, Cors,
+								Dotenv, Express.js, Node.js,MongoDB
+							</p>
+							<div className='card-actions flex justify-end'>
+								<button className='button'>
+									<a
+										target={'blank'}
+										href='https://github.com/MdHasan0467/Backend-and-database-integrate-client'
+									>
+										Source Code
+									</a>
+								</button>
+								<button className='button'>
 									<a
 										target={'blank'}
 										href='https://assignment-eleven-30dd0.web.app/'
@@ -28,9 +47,26 @@ const Projects = () => {
 							<img src='usedCarCover.png' alt='Shoes' />
 						</figure>
 						<div className='card-body'>
-							<p>A website for buying used car</p>
-							<div className='card-actions justify-end'>
-								<button className='btn btn-primary'>
+							<h1 className='text-lg text-yellow-400 text-start text-bold font-serif'>
+								A website for buying used car
+							</h1>
+							<h2 className='text-lg text-pink-500 text-start text-bold font-serif'>
+								Technologies used in this project:
+							</h2>
+							<p className='text-start'>
+								React, React-Tailwindcss, React Router, TanStack Query, Cors,
+								Dotenv, Express.js, Node.js,MongoDB
+							</p>
+							<div className='card-actions flex justify-end'>
+								<button className='button'>
+									<a
+										target={'blank'}
+										href='https://github.com/MdHasan0467/build-won-complete-project-client'
+									>
+										Source Code
+									</a>
+								</button>
+								<button className='button'>
 									<a
 										target={'blank'}
 										href='https://assignment-twelve-b5667.web.app/'
@@ -46,9 +82,26 @@ const Projects = () => {
 							<img src='courseCover.png' alt='Shoes' />
 						</figure>
 						<div className='card-body'>
-							<p>A website about online course</p>
-							<div className='card-actions justify-end'>
-								<button className='btn btn-primary'>
+							<h1 className='text-lg text-yellow-400 text-start text-bold font-serif'>
+								A website about online course
+							</h1>
+							<h2 className='text-lg text-pink-500 text-start text-bold font-serif'>
+								Technologies used in this project:
+							</h2>
+							<p className='text-start'>
+								React, React-Tailwindcss, React Router, TanStack Query, Cors,
+								Dotenv, Express.js
+							</p>
+							<div className='card-actions flex justify-end'>
+								<button className='button'>
+									<a
+										target={'blank'}
+										href='https://github.com/MdHasan0467/react-authentication-client'
+									>
+										Source Code
+									</a>
+								</button>
+								<button className='button'>
 									<a
 										target={'blank'}
 										href='https://assignment-ten-625db.web.app/courses'
