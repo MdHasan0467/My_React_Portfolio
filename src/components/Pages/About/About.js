70a1ca2d07301cc41dda8c71de1b97719e80f38a
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-12-10 23:02:30 +0600

    projects page skill and tool customize

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -22,29 +22,33 @@ const About = () => {
 									users will find easy to use and visually appealing.
 								</h2>
 							</div>
-							<div className='flex justify-around my-7'>
+							<div className='grid grid-cols-1 md:grid-cols-2 gap-10 my-7'>
 								<div>
-									<h1 className='text-5xl text-start font-bold'>My Skills</h1>
-									<ul className='my-5'>
-										<li className='text-start'>HTML5</li>
-										<li className='text-start'>CSS3</li>
-										<li className='text-start'>Tailwindcss</li>
-										<li className='text-start'>Bootstrap</li>
-										<li className='text-start'>JavaScript</li>
-										<li className='text-start'>React js</li>
-										<li className='text-start'>Node js</li>
-										<li className='text-start'>Firebase</li>
-										<li className='text-start'>MongoDB</li>
+									<h1 className='text-5xl text-start font-bold border border-gray-400 shadow-lg shadow-gray-500 p-3 rounded-lg'>
+										My Skills :
+									</h1>
+									<ul className='my-7'>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ HTML5</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ CSS3</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Tailwindcss</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Bootstrap</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ JavaScript</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ React js</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Node js</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Firebase</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ MongoDB</li>
 									</ul>
 								</div>
 								<div>
-									<h1 className='text-5xl text-start font-bold'>Tools</h1>
-									<ul className='my-5'>
-										<li className='text-start'>Chrome Dev Tool</li>
-										<li className='text-start'>Visual Studio Code</li>
-										<li className='text-start'>Netlify</li>
-										<li className='text-start'>github</li>
-										<li className='text-start'>Vercel</li>
+									<h1 className='text-5xl text-start font-bold border border-gray-400 shadow-lg shadow-gray-500 p-3 rounded-lg'>
+										Tools :
+									</h1>
+									<ul className='my-7'>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Chrome Dev Tool</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Visual Studio Code</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Netlify</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ github</li>
+										<li className='text-start hover:text-yellow-400 cursor-pointer'> ✅ Vercel</li>
 									</ul>
 								</div>
 							</div>
