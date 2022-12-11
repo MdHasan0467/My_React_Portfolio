4552f02b2a77fc72571dd3dacd1f1450a1bef3fc
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-12-12 02:43:07 +0600

    about page fade animation added

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
 
 const About = () => {
     return (
@@ -18,82 +19,84 @@ const About = () => {
 									users will find easy to use and visually appealing.
 								</h2>
 							</div>
-							<div className='grid grid-cols-1 md:grid-cols-3 gap-5 my-7'>
-								<img
-									src='my-desktop-pic.jpg'
-									className='lg:max-w-sm rounded-lg shadow-2xl'
-								/>
-								<div>
-									<h1 className='text-5xl text-start font-bold border border-gray-400 shadow-lg shadow-gray-500 p-3 rounded-lg'>
-										My Skills :
-									</h1>
-									<ul className='my-7'>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ HTML5
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ CSS3
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Tailwindcss
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Bootstrap
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ JavaScript
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ React js
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Node js
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Firebase
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ MongoDB
-										</li>
-									</ul>
+							<Fade left>
+								<div className='grid grid-cols-1 md:grid-cols-3 gap-5 my-7'>
+									<img
+										src='my-desktop-pic.jpg'
+										className='lg:max-w-sm rounded-lg shadow-2xl'
+									/>
+									<div>
+										<h1 className='text-5xl text-start font-bold border border-gray-400 shadow-lg shadow-gray-500 p-3 rounded-lg'>
+											My Skills :
+										</h1>
+										<ul className='my-7'>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ HTML5
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ CSS3
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Tailwindcss
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Bootstrap
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ JavaScript
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ React js
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Node js
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Firebase
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ MongoDB
+											</li>
+										</ul>
+									</div>
+									<div>
+										<h1 className='text-5xl text-start font-bold border border-gray-400 shadow-lg shadow-gray-500 p-3 rounded-lg'>
+											Tools :
+										</h1>
+										<ul className='my-7'>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Chrome Dev Tool
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Visual Studio Code
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Netlify
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ github
+											</li>
+											<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
+												{' '}
+												✅ Vercel
+											</li>
+										</ul>
+									</div>
 								</div>
-								<div>
-									<h1 className='text-5xl text-start font-bold border border-gray-400 shadow-lg shadow-gray-500 p-3 rounded-lg'>
-										Tools :
-									</h1>
-									<ul className='my-7'>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Chrome Dev Tool
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Visual Studio Code
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Netlify
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ github
-										</li>
-										<li className='text-start rounded-md hover:text-black hover:bg-yellow-50 p-2 cursor-pointer'>
-											{' '}
-											✅ Vercel
-										</li>
-									</ul>
-								</div>
-							</div>
+							</Fade>
 						</div>
 					</div>
 				</div>
